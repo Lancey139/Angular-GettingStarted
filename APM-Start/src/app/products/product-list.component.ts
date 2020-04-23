de6b29d06b87e6fa58443589bf63886fdb5566c7
MEO Nicolas <nicolas.meo@soprasteria.com>
2020-04-23 09:00:24 +0200

    Avancement dans cours pluralsight

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { IProduct } from './product';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   // Attributs
   pageTitle: string = 'Product List';
   imageWidth : number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+  filteredProducts: IProduct[];
   products: IProduct[] = [
     {
       "productId": 1,
@@ -37,9 +47,27 @@ export class ProductListComponent {
 
   // Méthodes
   // pas de mot clé () : return type 
+  constructor() {
+    // Initialisation des attributs
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
+
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
+
+  ngOnInit(): void {
+    console.log('In OnInit');
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);       
+  }
+
+
 }
 
 
---
+++ APM-Start/src/app/shared/star.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component, OnChanges, Input } from '@angular/core';
+
+@Component( {
+    selector: 'pm-start',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnChanges {
+    @Input() rating: number = 4;
+    starWidth: number;
+
+    ngOnChanges(): void {
+        this.starWidth = this.rating*75/5;
+    }
+}
